1ffe5908b3f0e129d32c314806d3552b768ad2ba
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-09-23 13:58:37 -0400

    Added test to see if I read a file right

--- test/read_test.cpp
+++ test/read_test.cpp
@@ -6,6 +6,7 @@ using namespace std;
 SCENARIO( "Make sure built-in file reader can parse known example file properly", "[reader]"){
 
   GIVEN( "An input file and the integrator's reader"){
+    const double EPSILON = 0.00001;//for double "comparisons"
     Langevin integrator;
     string filename = "pot_example.txt";
     integrator.read_file(filename);
@@ -13,6 +14,10 @@ SCENARIO( "Make sure built-in file reader can parse known example file properly"
       THEN("we should get correct array lengths"){
 	REQUIRE( sizeof(integrator.potentials) == 200);//known number of lines
 	REQUIRE( sizeof(integrator.forces) == 200);
+      }
+      AND_THEN("We should have the right numbers"){//just check a few points
+	REQUIRE( (integrator.forces[0] + 0.113475) < EPSILON );
+	RETUIRE( (integrator.potentials[50] - 0.012177) < EPSILON);
       }
     }
   }
