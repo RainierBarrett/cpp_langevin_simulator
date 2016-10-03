f9f35c5fc203ad55a0d25e6c143903af31bd77fb
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-10-03 16:19:32 -0400

    Trying again to get Traivs to run gcov

--- coverage.sh
+++ coverage.sh
@@ -3,6 +3,7 @@ printf "Testing langevin.cpp for coverage...\n"
 PROJECT_DIR=~/cpp_langevin_simulator
 GCOV_PATH=`locate -i langevin.cpp.gcno | sed -n 1p | grep -o ".*/"`
 printf "(gcov files can be found at ${GCOV_PATH})\n"
+sudo chmod a+rw $GCOV_PATH/langevin.cpp.gcov
 gcov $GCOV_PATH/langevin.cpp.gcov -n | grep langevin.cpp\' -A1
 PERCENT=`gcov $GCOV_PATH/langevin.cpp.gcov -n | grep langevin.cpp -A1| grep "[0-9]\+\.[0-9]\+\?" -o`
 sed -i -e "s/[0-9]\+\.[0-9]\+\%/${PERCENT}%/g" $PROJECT_DIR/README.md
