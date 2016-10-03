b64ac753aab127ea25a3f9e2662456d02442a622
Rainier Barrett <rbarret8@ur.rochester.edu>
2016-10-03 15:48:14 -0400

    Updated the coverage script so it should run on Travis now...

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 printf "Testing langevin.cpp for coverage...\n"
-PROJECT_DIR=~/cpp_langevin_simulator
-GCOV_PATH=build/src/CMakeFiles/sharedlib.dir
-printf "(gcov files can be found at ${PROJECT_DIR}/${GCOV_PATH})\n"
-gcov $PROJECT_DIR/$GCOV_PATH/langevin.cpp.gcov -n | grep langevin.cpp\' -A1
+#PROJECT_DIR=~/cpp_langevin_simulator
+GCOV_PATH=`locate -i langevin.cpp.gcno | sed -n 1p | grep -o ".*/"`
+printf "(gcov files can be found at ${GCOV_PATH})\n"
+gcov $GCOV_PATH/langevin.cpp.gcov -n | grep langevin.cpp\' -A1
 PERCENT=`gcov $PROJECT_DIR/$GCOV_PATH/langevin.cpp.gcov -n | grep langevin.cpp -A1| grep "[0-9]\+\.[0-9]\+\?" -o`
 sed -i -e "s/[0-9]\+\.[0-9]\+\%/${PERCENT}%/g" README.md
 
